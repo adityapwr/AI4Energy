da9e7a31c7e56b769fc47b113c7bc80b8b9efb96
Aditya Pawar <pawar.aditya29@gmail.com>
2023-05-20 15:28:39 +0530

    feat: sample model

--- data_engineering/SQL/demand_agg.sql
+++ data_engineering/SQL/demand_agg.sql
@@ -1,13 +1,13 @@
 INSERT INTO aggregated_demand (agg_timestamp, state_name, average_freq, average_demand)
 SELECT
-  (DATE_TRUNC('hour', current_datetime) + INTERVAL '15 minutes')::timestamp AS agg_timestamp,
+  (DATE_TRUNC('hour', current_datetime) + INTERVAL '15 minutes' + ((FLOOR(EXTRACT(MINUTE FROM current_datetime) / 15) * 15) || ' minutes')::interval) AS agg_timestamp,
   state_name,
   AVG(frequency) AS average_freq,
   AVG(demand) AS average_demand
 FROM
   demand
 GROUP BY
-  (DATE_TRUNC('hour', current_datetime) + INTERVAL '15 minutes')::timestamp,
+  (DATE_TRUNC('hour', current_datetime) + INTERVAL '15 minutes' + ((FLOOR(EXTRACT(MINUTE FROM current_datetime) / 15) * 15) || ' minutes')::interval),
   state_name
 ORDER BY
   agg_timestamp;
